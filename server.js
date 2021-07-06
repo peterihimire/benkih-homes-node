c38f3eff998419c3b506ef6932b7735677e09dcc
Eromosele Ihimire <eromoseleihimire@Eromoseles-MacBook-Pro.local>
2021-07-06 18:51:49 -0400

    Worked on the server file and created the controllers and routes folders with there individual files

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const express = require('express')
 const app = express()
 
 const PORT = 4000;
-app.listen( PORT,() => console.log(`Server is running on port ${PORT}`))
+app.listen( PORT,() => console.log(`Server is running on port ${PORT}`));
