1447f44881ff3adada383074a3ee27f9e38c58f5
Peter Ihimire <peterihimire@gmail.com>
2021-07-17 14:31:53 +0100

    Added C.O.R.S to the server

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ const app = express();
 // MIDDLEWARES
 app.use(bodyParser.json());
 
+// FOR C.O.R.S ERROR
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Accept, Origin, X-Requested-With, Authorization",
+  );
+  next();
+});
+
 // ROUTES MIDDLEWARE
 
 // => /api/users/
