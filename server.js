0decd9ef1e628dee551d04d7f3f2e4f0d1f31987
Peter Ihimire <peterihimire@gmail.com>
2021-08-16 00:34:45 +0100

    Modified the app file

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 // FOR C.O.R.S ERROR
 app.use(
   cors({
-    origin: ["https://bnk-homes.netlify.app", "http://localhost:3000"]
+    origin: ["https://bnk-homes.netlify.app", "http://localhost:3000"],
     methods: ["GET, POST, PUT, PATCH, DELETE, OPTIONS"],
     allowedHeaders: [
       "Content-Type,Accept, Origin, X-Requested-With, Authorization",
